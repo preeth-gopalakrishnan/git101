e400e0410ae2511def3904571385bacaf37bf27a
preeth-gopalakrishnan <preeth.gopalakrishnan@ibsplc.com>
2017-02-23 12:01:29 +0530

    First commit

---
+++ src/main/java/com/its/test/drop/HelloWorldApplication.java
@@ -0,0 +1,44 @@
+package com.its.test.drop;
+
+import com.its.test.drop.health.TemplateHealthCheck;
+import com.its.test.drop.resources.HelloWorldResource;
+
+import io.dropwizard.Application;
+import io.dropwizard.configuration.ResourceConfigurationSourceProvider;
+import io.dropwizard.configuration.UrlConfigurationSourceProvider;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
+
+	public static void main(final String[] args) throws Exception {
+		new HelloWorldApplication().run(args);
+	}
+
+	@Override
+	public String getName() {
+		return "Hello World";
+	}
+
+	@Override
+	public void initialize(final Bootstrap<HelloWorldConfiguration> bootstrap) {
+		// TODO: application initialization
+		bootstrap.setConfigurationSourceProvider(new ResourceConfigurationSourceProvider());
+	}
+	
+
+	@Override
+	public void run(final HelloWorldConfiguration configuration,
+			final Environment environment) {
+		final HelloWorldResource resource = new HelloWorldResource(
+				configuration.getTemplate(),
+				configuration.getDefaultName()
+				);
+		final TemplateHealthCheck healthCheck =
+				new TemplateHealthCheck(configuration.getTemplate());
+		environment.healthChecks().register("template", healthCheck);
+		environment.jersey().register(resource);
+		// TODO: implement application
+	}
+
+}
